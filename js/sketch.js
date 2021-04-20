0d53013f4635589aabcf78f769f50949552e6ea9
antoinechatelat <chatelat.antoine@gmail.com>
2021-04-20 21:36:53 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,15 @@
 
+let img;
+
+function preload () {
+  img = loadImage('../img/work2.jpeg');
+}
+
 function setup () {
   createCanvas (1350,600);
-  background('rgb(0,255,0)');
+}
 
+function drawn () {
+  background('rgba(100%,0%,100%,0.5)');
+  image(img, 0, 0);
 }
